50c918c9e774e466641fd2518ddf35f1465567ed
sudhanshu2070 <sssallen2016@gmail.com>
2025-03-03 23:31:29 +0530

    Added Appointment Screen 0.33

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,7 @@
+/**
+ * Defines the parameter list for the root stack navigator.
+ * Each screen in the stack is represented by a key, and the value is the type of the parameters expected by that screen.
+ */
 export type RootStackParamList = {
     LogoScreen: undefined;
     LoginScreen: undefined;
